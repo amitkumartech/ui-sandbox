28d37a17ce373e8a460c1aa3f50d7d542a6c24a2
Amit Kumar <verma.amit0407@gmail.com>
2024-12-11 20:22:39 +0530

    created application, installed my publish package ak-shared-components and consumed on component from the package

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ui-andbox' title`, () => {
+  it(`should have the 'ui-sandbox' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ui-andbox');
+    expect(app.title).toEqual('ui-sandbox');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ui-andbox');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, ui-sandbox'
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AkCardComponent } from 'ak-shared-components';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AkCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ui-andbox';
+  title = 'UI Sandbox';
 }
